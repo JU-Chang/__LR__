7afbfeda71fa52c82a66e2a32ef682d3efa966cd
JU <zhangyinzhu@qq.com>
2017-03-06 14:35:18 +0800

    chang the dirname of each video from number to its original one

--- extractFrames.py
+++ extractFrames.py
@@ -6,19 +6,16 @@ destination_path = '/media/chang/fe3dd8af-5577-42cb-95fb-4bd30a47cc9e/dataset/GR
 
 speaker_num = 34
 for files in os.listdir(source_path):
-    video_count = 0
     print('processing:%s' % files)
     for filename in os.listdir(os.path.join(source_path, files)):
-        print('video:%s' % video_count)
         if filename.endswith('.mpg'):
-            video_count += 1
             frame_count = 0
             vc = cv2.VideoCapture(os.path.join(source_path, files, filename))
             print cv2.cv.CV_CAP_PROP_FPS
             if vc.isOpened():
+                dest_dir = os.path.join(destination_path, files, str(filename.split('.', 1)[0]))
                 while True:
                     rval, frame = vc.read()
-                    dest_dir = os.path.join(destination_path, files, str(video_count))
                     if not os.path.exists(dest_dir):
                         os.makedirs(dest_dir)
                     jpg_filename = os.path.join(dest_dir, str(frame_count)+'.jpg')
